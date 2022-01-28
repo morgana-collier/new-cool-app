a7a6f4c76cfafeb34ccc1f4095db6e9c5ca500b8
gcode213 <98584379+gcode213@users.noreply.github.com>
2022-01-28 06:47:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 # Add your name in this list!
 trainees = [
-    "Harry",
+    "Gurj",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
-    "pizza",
+    "buffet",
 ]
 
 @app.route('/')
